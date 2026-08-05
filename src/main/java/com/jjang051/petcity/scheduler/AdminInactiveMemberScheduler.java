db2ac4912af3be0629a83db2914d8a2036329e5e
sejin <perudeim@gmail.com>
2026-08-05 12:37:48 +0900

    fix : 회원 삭제 버그 수정

--- src/main/java/com/jjang051/petcity/scheduler/AdminInactiveMemberScheduler.java
+++ src/main/java/com/jjang051/petcity/scheduler/AdminInactiveMemberScheduler.java
@@ -13,13 +13,13 @@ public class AdminInactiveMemberScheduler {
 
     private final AdminInactiveMemberService adminInactiveMemberService;
 
-    @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul")
+//    @Scheduled(cron = "0 0 3 * * *", zone = "Asia/Seoul")
     //위에는 새벽 3시마다 작동 아래는 연습용 (테스트 완료 후 아래는 끄세요)
-//    @Scheduled(fixedDelay = 10000)
+    @Scheduled(fixedDelay = 10000)
     public void blockInactiveMembers() {
-        int hardDeletedCount = adminInactiveMemberService.hardDeleteExpiredInactiveMembers();
         int deletedCount = adminInactiveMemberService.markBlockedMembersAsDeleted();
         int blockedCount = adminInactiveMemberService.blockInactiveMembers();
+        int hardDeletedCount = adminInactiveMemberService.hardDeleteExpiredInactiveMembers();
 
         log.info(
                 "장기 미로그인 회원 처리 완료 - 차단: {}명, 삭제 상태 전환: {}명, 하드 삭제: {}명",
